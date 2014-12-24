dbadde5ed5739e3f641eaaad30adbbc20147e415
Gabriel Adomnicai <gabesoft@gmail.com>
2014-12-23 22:39:18 -0800

    cleanup

--- components/auth/broc/sass-compile.js
+++ components/auth/broc/sass-compile.js
@@ -1,46 +1,49 @@
 'use strict';
 
+// TODO: set paths and env in one place
+
 var pickFiles   = require('broccoli-static-compiler')
   , compileSass = require('broccoli-sass')
+  , prod = process.env.BRCOCCOLI_ENV === 'production'
   , mergeTrees  = require('broccoli-merge-trees')
   , trees       = {
         app   : 'app'
       , bower : '../../bower_components'
     };
 
-trees.vendor = pickFiles(trees.bower, {
+trees.vendorSass = pickFiles(trees.bower, {
     srcDir  : '/foundation/scss'
   , destDir : '/foundation'
 });
 
-trees.fonts = pickFiles(trees.bower, {
+trees.fontsSass = pickFiles(trees.bower, {
     srcDir  : '/font-awesome/scss'
   , destDir : '/font-awesome'
 });
-
-trees.lib = pickFiles(trees.app, {
-    srcDir  : '/styles'
-  , destDir : '/styles'
-});
-
-trees.compiled = compileSass([ trees.vendor, trees.fonts, trees.lib ]
-  , '/styles/app.scss'
-  , '/app.css'
-  , {
-        sourceMap      : true
-      //, outputStyle    : 'compressed'
-      , sourceComments : 'map'
-    });
 
 trees.fontsDir = pickFiles(trees.bower, {
     srcDir  : '/font-awesome/fonts'
   , destDir : '/assets/auth/fonts'
 });
 
-trees.output = pickFiles(trees.compiled, {
+trees.appSass = pickFiles(trees.app, {
+    srcDir  : '/styles'
+  , destDir : '/styles'
+});
+
+trees.compiled = compileSass([ trees.vendorSass, trees.fontsSass, trees.appSass ]
+  , '/styles/app.scss'
+  , '/app.css'
+  , {
+        sourceMap      : true
+      , outputStyle    : prod ? 'compressed' : 'inline'
+      , sourceComments : 'map'
+    });
+
+trees.outputCss = pickFiles(trees.compiled, {
     srcDir  : '/'
   , destDir : '/assets/auth/css/'
   , files   : [ 'app.css', 'app.css.map' ]
 });
 
-module.exports = mergeTrees([ trees.output, trees.fontsDir ]);
+module.exports = mergeTrees([ trees.outputCss, trees.fontsDir ]);
