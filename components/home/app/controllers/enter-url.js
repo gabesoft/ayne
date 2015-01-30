e3dc6dd0f407e1d0a747b4b0b48c462fb41a6054
Gabriel Adomnicai <gabesoft@gmail.com>
2015-01-29 23:25:16 -0800

    enter url html

--- components/home/app/controllers/enter-url.js
+++ components/home/app/controllers/enter-url.js
@@ -2,5 +2,14 @@ import Validator from 'core/app/mixins/validator';
 import Legend from 'user/app/mixins/legend';
 
 export default Ember.ObjectController.extend(Validator, Legend, {
-
+    model: {}
+  , favicon: function () {
+        var href = this.get('model.href');
+        return 'http://www.google.com/s2/favicons?domain_url=' + encodeURIComponent(href);
+    }.property('model.href')
+  , name: function () {
+        // TODO: use title here
+        var href = this.get('model.href');
+        return (href || '').toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+    }.property('model.href')
 });
