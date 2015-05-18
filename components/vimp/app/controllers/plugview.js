2c66e6dff2870e521160c428f872451a835a1e09
Gabriel Adomnicai <gabesoft@gmail.com>
2015-05-18 08:29:29 -0700

    Show author name instead of login

--- components/vimp/app/controllers/plugview.js
+++ components/vimp/app/controllers/plugview.js
@@ -1,1 +1,5 @@
-export default Ember.Controller.extend({});
+export default Ember.Controller.extend({
+    author : function () {
+        return this.get('model.author.name') || this.get('model.author.login');
+    }.property('model')
+});
