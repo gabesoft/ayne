1258249a37d899e51fa617903cd5fe51305ec5be
Gabriel Adomnicai <gabesoft@gmail.com>
2015-06-04 22:57:01 -0700

    Display fixes

--- components/vimp/app/controllers/details-dialog.js
+++ components/vimp/app/controllers/details-dialog.js
@@ -11,11 +11,11 @@ export default Ember.Controller.extend({
         return this.get('model.hasReadme') && this.get('model.hasDoc');
     }.property('model.hasReadme', 'model.hasDoc')
   , modalSize : function () {
-        return this.get('model.hasReadme') ? 'full' : 'tiny';
+        return this.get('hasReadmeOrDoc') ? 'full' : 'tiny';
     }.property('model.hasReadme')
   , closeTimeout : function () {
-        return this.get('model.hasReadme') ? 0 : 1000;
-    }.property('model.hasReadme')
+        return this.get('hasReadmeOrDoc') ? 0 : 1000;
+    }.property('hasReadmeOrDoc')
   , actions : {
         closeModal : function () {
             return this.send('hideModal');
