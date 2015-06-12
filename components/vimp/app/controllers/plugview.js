b47aac8e8ce22105f99761abc64a533233359dc0
Gabriel Adomnicai <gabesoft@gmail.com>
2015-06-12 08:57:16 -0700

    Style fixes

--- components/vimp/app/controllers/plugview.js
+++ components/vimp/app/controllers/plugview.js
@@ -4,4 +4,7 @@ export default Ember.Controller.extend({
         || this.get('model.author.login')
         || this.get('model.author');
     }.property('model')
+  , aboveThreshold : function () {
+        return this.get('model.isPlugin') > 0.3;
+    }.property('isPlugin')
 });
