20ff159cd787da2f22e271059d2918006822853a
Gabriel Adomnicai <gabesoft@gmail.com>
2015-07-31 22:48:24 -0700

    Ember migration to 13.5 - vimp components complete

--- components/vimp/app/controllers/plug-detail.js
+++ components/vimp/app/controllers/plug-detail.js
@@ -1,1 +1,17 @@
-export default Ember.Controller.extend({});
+export default Ember.Controller.extend({
+    hasReadmeOrDoc: function () {
+        return this.get('model.hasReadme') || this.get('model.hasDoc');
+    }.property('model'),
+    hasReadmeAndDoc: function () {
+        return this.get('model.hasReadme') && this.get('model.hasDoc');
+    }.property('model'),
+    noReadmeOrDoc: function () {
+        return !this.get('hasReadmeOrDoc');
+    }.property('hasReadmeOrDoc'),
+    hasOnlyDoc: function () {
+        return this.get('model.hasDoc') && !this.get('model.hasReadme');
+    }.property('model'),
+    hasOnlyReadme: function () {
+        return this.get('model.hasReadme') && !this.get('model.hasDoc');
+    }.property('model')
+});
--- components/vimp/app/routes/index.js
+++ components/vimp/app/routes/index.js
@@ -1,51 +1,48 @@
 function makeHash(promises) {
     var hash = Object.keys(promises).reduce(function (acc, name) {
-            acc[name] = promises[name]()
-               .then(function (response) { return response.data || []; })
-               .catch(function () { return []; });
-            return acc;
-        }, {});
+        acc[name] = promises[name]()
+            .then(function (response) {return response.data || [];})
+            .catch(function () {return [];});
+        return acc;
+    }, {});
 
     return Ember.RSVP.hash(hash);
 }
 
 export default Ember.Route.extend({
-    queryParams : {
-        search : {
-            refreshModel : true
-          , replace      : false
+    queryParams: {
+        search: {
+            refreshModel: true,
+            replace: false
         }
-    }
-  , model : function (params) {
+    },
+    model: function (params) {
         return makeHash({
-            plugins     : this.api.getVplugs.bind(this.api, params)
-          , mostStarred : this.api.getMostStarred.bind(this.api)
-          , lastUpdated : this.api.getLastUpdated.bind(this.api)
+            plugins: this.api.getVplugs.bind(this.api, params),
+            mostStarred: this.api.getMostStarred.bind(this.api),
+            lastUpdated: this.api.getLastUpdated.bind(this.api)
         });
-    }
-  , actions : {
-        showModal : function (name, model) {
+    },
+    actions: {
+        showModal: function (name, model) {
             return this.api.getVplug(model.id)
-               .then(function (response) {
+                .then(function (response) {
                     return this.render(name, {
-                        outlet : 'modal'
-                      , into   : 'application'
-                      , model  : response.data
+                        outlet: 'modal',
+                        into: 'application',
+                        model: response.data
                     });
                 }.bind(this));
-        }
-      , hideModal : function () {
-            function removeHash () {
-                history.pushState(""
-                  , document.title
-                  , window.location.pathname + window.location.search
-                );
+        },
+        hideModal: function () {
+            function removeHash() {
+                history.pushState("", document.title, window.location.pathname + window.location.search);
             }
 
             removeHash();
             return this.disconnectOutlet({
-                outlet     : 'modal'
-              , parentView : 'application'
+                outlet: 'modal',
+                parentView: 'application'
             });
         }
     }
