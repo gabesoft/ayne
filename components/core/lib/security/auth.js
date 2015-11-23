6d89fea4e48d02513ea4016525cd2e995b9a479c
Gabriel Adomnicai <gabesoft@gmail.com>
2015-11-22 21:35:15 -0800

    Add a type with the user data

--- components/core/lib/security/auth.js
+++ components/core/lib/security/auth.js
@@ -1,125 +1,152 @@
-var api               = require('../api')
-  , UnauthorizedError = require('../errors/unauthorized')
-  , token             = require('./token')
-  , EmailExistsError  = require('../errors/email-exists')
-  , TokenExpiredError = require('../errors/token-expired')
-  , moment            = require('moment')
-  , conf              = require('../../../../config/store')
-  , ttlGuid           = moment.duration(conf.get('user-guid:ttl:value'), conf.get('user-guid:ttl:unit'))
-  , bcrypt            = require('bcrypt');
+var api = require('../api'),
+    UnauthorizedError = require('../errors/unauthorized'),
+    token = require('./token'),
+    EmailExistsError = require('../errors/email-exists'),
+    TokenExpiredError = require('../errors/token-expired'),
+    moment = require('moment'),
+    conf = require('../../../../config/store'),
+    ttlGuid = moment.duration(conf.get('user-guid:ttl:value'), conf.get('user-guid:ttl:unit')),
+    bcrypt = require('bcrypt');
 
-function hashPassword (password, cb) {
-    bcrypt.genSalt(11, function (err, salt) {
-        if (err) { return cb(err); }
+function hashPassword(password, cb) {
+  bcrypt.genSalt(11, function (err, salt) {
+    if (err) {
+      return cb(err);
+    }
 
-        bcrypt.hash(password || '', salt, function (err, hash) {
-            cb(err, hash);
-        });
+    bcrypt.hash(password || '', salt, function (err, hash) {
+      cb(err, hash);
     });
+  });
 }
 
-function makeClientUser (user) {
-    return {
-        id    : user.id
-      , email : user.email
-    };
+function makeClientUser(user) {
+  return {
+    id: user.id,
+    email: user.email
+  };
 }
 
-function createUser (data, cb) {
-    hashPassword(data.password, function (err, hash) {
-        if (err) { return cb(err); }
+function createUser(data, cb) {
+  hashPassword(data.password, function (err, hash) {
+    if (err) {
+      return cb(err);
+    }
 
-        data.password = hash;
+    data.password = hash;
+    data.type = 'ayne';
 
-        api.post('/users', data, function (err, response, body) {
-            if (err && err.statusCode === 409) {
-                return cb(new EmailExistsError(data.email));
-            } else if (err) {
-                return cb(err);
-            }
+    api.post('/users', data, function (err, response, body) {
+      if (err && err.statusCode === 409) {
+        return cb(new EmailExistsError(data.email));
+      } else if (err) {
+        return cb(err);
+      }
 
-            cb(null, makeClientUser(body));
-        });
+      cb(null, makeClientUser(body));
     });
+  });
 }
 
-function loginUserWithGuid (guid, cb) {
-    api.get('/users', { guid: guid }, function (err, response, user) {
-        if (err || !user) { return cb(new TokenExpiredError()); }
+function loginUserWithGuid(guid, cb) {
+  api.get('/users', {
+    guid: guid,
+    type: 'ayne'
+  }, function (err, response, user) {
+    if (err || !user) {
+      return cb(new TokenExpiredError());
+    }
 
-        token.temp(user, function (err, tk) {
-            return err ? cb(err) : cb(null, {
-                user     : makeClientUser(user)
-              , token    : tk
-              , noVerify : true
-            });
-        });
+    token.temp(user, function (err, tk) {
+      return err ? cb(err) : cb(null, {
+        user: makeClientUser(user),
+        token: tk,
+        noVerify: true
+      });
     });
+  });
 }
 
-function loginUser (data, cb) {
-    verifyUser(data.user, function (err, user) {
-        if (err) { return cb(err); }
+function loginUser(data, cb) {
+  verifyUser(data.user, function (err, user) {
+    if (err) {
+      return cb(err);
+    }
 
-        token.make(user, function (err, tk) {
-            if (err) { return cb(err); }
+    token.make(user, function (err, tk) {
+      if (err) {
+        return cb(err);
+      }
 
-            cb(null, {
-                user  : user
-              , token : tk
-            });
-        });
+      cb(null, {
+        user: user,
+        token: tk
+      });
     });
+  });
 }
 
-function logoutUser (data, cb) {
-    token.remove(data.user, cb);
+function logoutUser(data, cb) {
+  token.remove(data.user, cb);
 }
 
-function verifyUser (data, cb) {
-    api.get('/users', { email: data.email }, function (err, response, body) {
-        if (err && err.statusCode === 404) { return cb(new UnauthorizedError()); }
-        if (err) { return cb(err); }
-        if (body.length !== 1) { return cb(new UnauthorizedError()); }
+function verifyUser(data, cb) {
+  api.get('/users', {
+    email: data.email,
+    type: 'ayne'
+  }, function (err, response, body) {
+    if (err && err.statusCode === 404) {
+      return cb(new UnauthorizedError());
+    }
+    if (err) {
+      return cb(err);
+    }
+    if (body.length !== 1) {
+      return cb(new UnauthorizedError());
+    }
 
-        body = body[0];
-        bcrypt.compare(data.password || '', body.password, function (err, match) {
-            if (err) { return cb(err); }
-            if (!match) { return cb(new UnauthorizedError()); }
+    body = body[0];
+    bcrypt.compare(data.password || '', body.password, function (err, match) {
+      if (err) {
+        return cb(err);
+      }
+      if (!match) {
+        return cb(new UnauthorizedError());
+      }
 
-            cb(null, makeClientUser(body));
-        });
+      cb(null, makeClientUser(body));
     });
+  });
 }
 
-function generateUUID(){
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = (d + Math.random()*16)%16 | 0;
-            d = Math.floor(d/16);
-            return (c === 'x' ? r : (r&0x3|0x8)).toString(16);
-        });
-    return uuid;
+function generateUUID() {
+  var d = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+  return uuid;
 }
 
-function linkUser (user, cb) {
-    var guid = generateUUID();
-    api.put('/users/link', {
-        guid         : guid
-      , ttlInSeconds : ttlGuid.asSeconds()
-      , userId       : user.id
-    }, function (err) {
-        cb(err, guid);
-    });
+function linkUser(user, cb) {
+  var guid = generateUUID();
+  api.put('/users/link', {
+    guid: guid,
+    ttlInSeconds: ttlGuid.asSeconds(),
+    userId: user.id
+  }, function (err) {
+    cb(err, guid);
+  });
 }
 
 
 module.exports = {
-    hashPassword      : hashPassword
-  , verifyUser        : verifyUser
-  , loginUser         : loginUser
-  , loginUserWithGuid : loginUserWithGuid
-  , logoutUser        : logoutUser
-  , createUser        : createUser
-  , linkUser          : linkUser
+  hashPassword: hashPassword,
+  verifyUser: verifyUser,
+  loginUser: loginUser,
+  loginUserWithGuid: loginUserWithGuid,
+  logoutUser: logoutUser,
+  createUser: createUser,
+  linkUser: linkUser
 };
--- components/user/api/handlers/password.js
+++ components/user/api/handlers/password.js
@@ -1,89 +1,104 @@
-var api               = require('../../../core/lib/api')
-  , GmailApi          = require('../../../core/lib/gmail').GmailApi
-  , gapi              = new GmailApi()
-  , async             = require('async')
-  , UserNotFoundError = require('../../../core/lib/errors/user-not-found')
-  , auth              = require('../../../core/lib/security/auth');
+var api = require('../../../core/lib/api'),
+    GmailApi = require('../../../core/lib/gmail').GmailApi,
+    gapi = new GmailApi(),
+    async = require('async'),
+    UserNotFoundError = require('../../../core/lib/errors/user-not-found'),
+    auth = require('../../../core/lib/security/auth');
 
-function updateUser (request, reply, user) {
-    auth.hashPassword(request.payload.password, function (err, hash) {
-        if (err) { return reply.boom(err); }
-
-        api.patch([ '/users', user.id ], { password: hash }, function (err, response, body) {
-            delete body.password;
-            reply(body);
-        });
-    });
-}
-
-function update (request, reply) {
-    var payload  = request.payload
-      , noVerify = request.auth.credentials.temp
-      , email    = request.auth.credentials.email
-      , userId   = request.auth.credentials.id;
-
-    if (noVerify) {
-        return updateUser(request, reply, { id: userId, email: email });
+function updateUser(request, reply, user) {
+  auth.hashPassword(request.payload.password, function (err, hash) {
+    if (err) {
+      return reply.boom(err);
     }
 
-    auth.loginUser({
-        user    : { email: email, password: payload.oldPassword }
-      , headers : request.headers
-    }, function (err, data) {
-        if (err) {
-            reply.boom(err);
-        } else {
-            updateUser(request, reply, data.user);
-        }
+    api.patch(['/users', user.id], {
+      password: hash
+    }, function (err, response, body) {
+      delete body.password;
+      reply(body);
     });
+  });
 }
 
-function sendEmail (request, reply, user, cb) {
-    async.waterfall([
-        function (next) {
-            auth.linkUser(user, next);
-        }
-      , function (guid, next) {
-            var host = reply.conf('app:url')
-              , url  = [ host, 'user', 'reset-password', guid ].join('/');
-            request.server.render('user/templates/reset-password-email.jade', {
-                host     : host
-              , resetUrl : url
-            }, function (err, body) {
-                next(err, body);
-            });
-        }
-      , function (body, next) {
-            gapi.sendEmail({
-                to      : user.email
-              , subject : 'Reset password'
-              , body    : body
-            }, next);
-        }
-    ], cb);
+function update(request, reply) {
+  var payload = request.payload,
+      noVerify = request.auth.credentials.temp,
+      email = request.auth.credentials.email,
+      userId = request.auth.credentials.id;
+
+  if (noVerify) {
+    return updateUser(request, reply, {
+      id: userId,
+      email: email
+    });
+  }
+
+  auth.loginUser({
+    user: {
+      email: email,
+      password: payload.oldPassword
+    },
+    headers: request.headers
+  }, function (err, data) {
+    if (err) {
+      reply.boom(err);
+    } else {
+      updateUser(request, reply, data.user);
+    }
+  });
 }
 
-function sendResetEmail (request, reply) {
-    var data = request.payload;
+function sendEmail(request, reply, user, cb) {
+  async.waterfall([
+    function (next) {
+      auth.linkUser(user, next);
+    }
+    ,
+    function (guid, next) {
+      var host = reply.conf('app:url'),
+          url = [host, 'user', 'reset-password', guid].join('/');
+      request.server.render('user/templates/reset-password-email.jade', {
+        host: host,
+        resetUrl: url
+      }, function (err, body) {
+        next(err, body);
+      });
+    }
+    ,
+    function (body, next) {
+      gapi.sendEmail({
+        to: user.email,
+        subject: 'Reset password',
+        body: body
+      }, next);
+    }
+  ], cb);
+}
 
-    api.get('/users', { email: data.email }, function (err, response, body) {
-        if (err) {
-            reply.boom(err);
-        } else if (!body || body.length === 0) {
-            reply.boom(new UserNotFoundError(data.email, 'email'));
-        } else {
-            body = body[0];
-            sendEmail(request, reply, body, function (err) {
-                return err ? reply.boom(err) : reply({
-                    status : 'email-sent'
-                  , email  : body.email
-                });
-            });
-        }
-    });
+function sendResetEmail(request, reply) {
+  var data = request.payload;
+
+  api.get('/users', {
+    email: data.email,
+    type: 'ayne'
+  }, function (err, response, body) {
+    if (err) {
+      reply.boom(err);
+    } else if (!body || body.length === 0) {
+      reply.boom(new UserNotFoundError(data.email, 'email'));
+    } else {
+      body = body[0];
+      sendEmail(request, reply, body, function (err) {
+        return err ? reply.boom(err) : reply({
+          status: 'email-sent',
+          email: body.email
+        });
+      });
+    }
+  });
 }
 
 module.exports = {
-    update         : update
-  , sendResetEmail : sendResetEmail
+  update: update,
+  sendResetEmail: sendResetEmail
 };
